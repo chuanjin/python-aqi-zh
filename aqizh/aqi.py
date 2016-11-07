a18f5d854d6cdff57e5a18f49ed012d70f265a5f
Chuan Jin <chuan.jin.813@gmail.com>
2016-11-07 22:19:46 +0100

    Add missing return in each function.

--- aqizh/aqi.py
+++ aqizh/aqi.py
@@ -31,6 +31,7 @@ class AQI(object):
     def params(self, p):
         self._params = {'token': self._token}
         if p:
+            del p['interface']
             station_code = p.get('station_code')
             city = p.get('city')
             self.validate_compulsory(city, station_code)
@@ -66,46 +67,41 @@ class AQI(object):
         except RequestException:
             raise NetworkException
 
-    # def __get(self, city, avg, stations, interface):
-    #     self.interface = interface
-    #     self.params = {'city': city, 'avg': avg, 'stations': stations}
-    #     return self.__base_get(self.params)
-
     def __get(self, **p):
         self.interface = p.get('interface')
         self.params = p
         return self.__base_get(self.params)
 
     def get_pm25(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/pm2_5')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/pm2_5')
 
     def get_pm10(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/pm10')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/pm10')
 
     def get_co(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/co')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/co')
 
     def get_no2(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/no2')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/no2')
 
     def get_so2(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/so2')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/so2')
 
     def get_o3(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/o3')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/o3')
 
     def get_aqi_details(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/aqi_details')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/aqi_details')
 
     def get_only_aqi(self, city, avg='true', stations='yes'):
-        self.__get(city=city, avg=avg, stations=stations, interface='/only_aqi')
+        return self.__get(city=city, avg=avg, stations=stations, interface='/only_aqi')
 
     def get_aqis_by_station(self, station_code):
-        self.__get(station_code=station_code, interface='/aqis_by_station')
+        return self.__get(station_code=station_code, interface='/aqis_by_station')
 
     def get_station_names(self, city):
         self.interface = '/station_names'
-        self.__get(city=city, interface='/station_names')
+        return self.__get(city=city, interface='/station_names')
 
     def get_cities(self):
         self.interface = ''
